da298ff275e93af9b33569c1ab005281fc6a7565
DerekDardzinski <dardzinski.derek@gmail.com>
2021-01-15 15:26:25 -0500

    Working on the InterfaceGenerator function

--- core/build.py
+++ core/build.py
@@ -1,21 +1,125 @@
 """
 This module will be used to construct the surfaces and interfaces used in this package.
 """
-from pymatgen.core.structure import Structure
+from pymatgen.core.structure import Structure 
 from pymatgen.io.vasp.inputs import Poscar
 from pymatgen.io.ase import AseAtomsAdaptor
 from pymatgen.symmetry.analyzer import SpacegroupAnalyzer
+from pymatgen.analysis.substrate_analyzer import ZSLGenerator, SubstrateAnalyzer
 from ase import Atoms
 from ase.io import read
 from ase.build.surfaces_with_termination import surfaces_with_termination
 from ase.spacegroup import get_spacegroup
+from ase.geometry import get_layers
+from ase.build.general_surface import ext_gcd
 import numpy as np
+from math import gcd
 from itertools import combinations
 
 
 class Surface:
     """
-    The surface classes generates surfaces with all possible terminations and contains 
+    The Surface class is a container for surfaces generated with the SurfaceGenerator class
+    and will be used as an input to the InterfaceGenerator class
+    """
+
+    def __init__(
+        self,
+        slab,
+        bulk,
+        miller_index,
+        layers,
+        vacuum,
+    ):
+        if type(slab) == Atoms:
+            self.slab_ase = slab
+            self.slab_pmg = AseAtomsAdaptor.get_structure(slab)
+        elif type(slab) == Structure:
+            self.slab_ase = AseAtomsAdaptor.get_atoms(slab)
+            self.slab_pmg = slab
+        else:
+            raise TypeError(f"structure accepts 'pymatgen.core.structure.Structure' or 'ase.Atoms' not '{type(slab).__name__}'")
+
+        if type(bulk) == Atoms:
+            self.bulk_ase = bulk
+            self.bulk_pmg = AseAtomsAdaptor.get_structure(bulk)
+        elif type(bulk) == Structure:
+            self.bulk_ase = AseAtomsAdaptor.get_atoms(bulk)
+            self.bulk_pmg = bulk
+        else:
+            raise TypeError(f"structure accepts 'pymatgen.core.structure.Structure' or 'ase.Atoms' not '{type(bulk).__name__}'")
+
+        self.miller_index = miller_index
+        self.layers = layers
+        self.vacuum = vacuum
+        self.termination = self._get_termination()
+        self.crystallographic_directions = self._get_crystallographic_directions()
+
+
+    def _get_termination(self):
+        z_layers, _ = get_layers(self.slab_ase, (0, 0, 1))
+        top_layer = [i for i, val in enumerate(z_layers == max(z_layers)) if val]
+        termination = np.unique(
+            [self.slab_ase.get_chemical_symbols()[a] for a in top_layer]
+        )
+
+        return termination
+
+    def _get_crystallographic_directions(self, tol=1e-10):
+        """
+        TODO: Figure out how to find the crystallographic directions of the a and b
+        lattice vectors in the primitive slab
+        """
+        indices = np.asarray(self.miller_index)
+
+        if indices.shape != (3,) or not indices.any() or indices.dtype != int:
+            raise ValueError('%s is an invalid surface type' % indices)
+
+        h, k, l = indices
+        h0, k0, l0 = (indices == 0)
+
+        if h0 and k0 or h0 and l0 or k0 and l0:  # if two indices are zero
+            if not h0:
+                c1, c2, c3 = [(0, 1, 0), (0, 0, 1), (1, 0, 0)]
+            if not k0:
+                c1, c2, c3 = [(0, 0, 1), (1, 0, 0), (0, 1, 0)]
+            if not l0:
+                c1, c2, c3 = [(1, 0, 0), (0, 1, 0), (0, 0, 1)]
+        else:
+            p, q = ext_gcd(k, l)
+            a1, a2, a3 = self.bulk_ase.cell
+
+            # constants describing the dot product of basis c1 and c2:
+            # dot(c1,c2) = k1+i*k2, i in Z
+            k1 = np.dot(p * (k * a1 - h * a2) + q * (l * a1 - h * a3),
+                        l * a2 - k * a3)
+            k2 = np.dot(l * (k * a1 - h * a2) - k * (l * a1 - h * a3),
+                        l * a2 - k * a3)
+
+            if abs(k2) > tol:
+                i = -int(round(k1 / k2))  # i corresponding to the optimal basis
+                p, q = p + i * l, q - i * k
+
+            a, b = ext_gcd(p * k + q * l, h)
+
+
+            c1 = (p * k + q * l, -p * h, -q * h)
+            c2 = np.array((0, l, -k)) // abs(gcd(l, k))
+            c3 = (b, a * p, a * q)
+
+        #  print(np.matmul(
+            #  np.linalg.inv(self.slab_basis), structure.lattice.matrix.T).T /
+            #  self.structure.cell[0][0]
+        #  )
+
+
+        return np.array([c1, c2, c3])
+
+
+
+class SurfaceGenerator:
+    """
+    The SurfaceGenerator classes generates surfaces with all possible terminations and contains 
     information about the Miller indices of the surface and the number of different
     terminations.
 
@@ -33,7 +137,6 @@ class Surface:
         layers,
         vacuum,
     ):
-        """TODO: to be defined. """
         if type(structure) == Atoms:
             self.structure = structure
         elif type(structure) == Structure:
@@ -44,9 +147,7 @@ class Surface:
         self.miller_index = miller_index
         self.layers = layers
         self.vacuum = vacuum
-        self.slabs_pmg = self._generate_slabs()
-        self.slabs_ase = [AseAtomsAdaptor.get_atoms(slab) for slab in self.slabs_pmg]
-        self.number_of_terminations = len(self.slabs_pmg)
+        self.slabs = self._generate_slabs()
 
     @classmethod
     def from_file(
@@ -67,6 +168,7 @@ class Surface:
 
         spacegroup = SpacegroupAnalyzer(structure)
         primitive = spacegroup.get_primitive_standard_structure()
+
 
         return primitive.get_sorted_structure()
 
@@ -96,12 +198,84 @@ class Surface:
             primitive_slabs[i].get_sorted_structure() for i in unique_slab_indices
         ]
 
-        return unique_primitive_slabs
+        surfaces = []
+        for slab in unique_primitive_slabs:
+            surface = Surface(
+                slab=slab,
+                bulk=self.structure,
+                miller_index=self.miller_index,
+                layers=self.layers,
+                vacuum=self.vacuum,
+            )
+            surfaces.append(surface)
+
+
+        return surfaces
+
+
+class InterfaceGenerator:
+    """
+    This class will use the lattice matching algorithm from Zur and McGill to generate
+    commensurate interface structures between two inorganic crystalline materials.
+    """
+
+    def __init__(
+            self,
+            substrate,
+            film,
+            area_tol=0.01,
+            angle_tol=0.01,
+            length_tol=0.01,
+            max_area=500,
+    ):
+        if type(substrate) == Surface:
+            self.substrate = substrate
+        else:
+            raise TypeError(f"InterfaceGenerator accepts 'ogre.core.Surface' not '{type(substrate).__name__}'")
+
+        if type(film) == Surface:
+            self.film = film
+        else:
+            raise TypeError(f"InterfaceGenerator accepts 'ogre.core.Surface' not '{type(film).__name__}'")
+
+        self.area_tol = area_tol
+        self.angle_tol = angle_tol
+        self.length_tol = length_tol
+        self.max_area = max_area
+
+
+        def _generate_interfaces(self):
+            ZSL = ZSLGenerator(
+                max_area_ratio_tol=self.area_tol,
+                max_angle_tol=self.angle_tol,
+                max_length_tol=self.length_tol,
+                max_area=self.max_area,
+            )
+
+            SA = SubstrateAnalyzer(zslgen=ZSL)
+
+            M = SA.calculate(
+                film=self.film.slab_ase,
+                substrate=self.substrate.slab_ase,
+                film_millers=[self.film.miller_index],
+                substrate_millers=[self.substrate.miller_index],
+            )
+
+        
 
 
 if __name__ == "__main__":
-    s = Surface.from_file('./POSCAR_InAs_conv', miller_index=[1,1,0], layers=5, vacuum=10)
-    print(s.slabs_ase[0])
+    s = SurfaceGenerator.from_file(
+        './POSCAR_InAs_conv',
+        miller_index=[0,1,1],
+        layers=5,
+        vacuum=10
+    )
+    print(s.slabs[0].termination)
+    #  print(np.matmul(s.slabs_pmg[0].lattice.matrix, np.linalg.inv(s.get_basis())))
+    #  bulk = AseAtomsAdaptor.get_structure(s.structure).lattice.matrix
+    #  slab = s.slabs_pmg[0].lattice.matrix
+    #  print(np.matmul(np.linalg.inv(slab), bulk))
     
 
 
